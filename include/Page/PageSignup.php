9cafed184768cb4bb1e211f701e7399df94717e8
JamesAttree <ja158@hw.ac.uk>
2012-03-25 13:16:16 +0200

    Update include/Page/PageSignup.php

--- include/Page/PageSignup.php
+++ include/Page/PageSignup.php
@@ -4,8 +4,19 @@ if(!defined("HMS")) die("Invalid entry point");
 
 class PageSignup extends PageBase
 {
-	protected function runPage()
-	{
+	protected function runPage(){
 		
+		$this->mBasePage = "book.tpl";
+
+		global $cWebPath;
+
+		$this->mSmarty->assign("valQbTitle", "");
+		$this->mSmarty->assign("valQbFisrtName", "");
+		$this->mSmarty->assign("valQbLastName", "");
+		$this->mSmarty->assign("valQbAddress", "");
+		$this->mSmarty->assign("valQbAddress2", "");
+		$this->mSmarty->assign("valQbPostCode", "");
+		$this->mSmarty->assign("valQbEmail", "");
+
 	}
 }
